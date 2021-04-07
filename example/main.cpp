a22f771feb5297b311df183eaccb1719d8351250
Beta Jester <betajester@betajester.com>
2021-04-07 22:20:31 +1000

    Add project files.

---
+++ example/main.cpp
@@ -0,0 +1,38 @@
+#include <fmt/ostream.h>
+#include <rustpp/rustpp.hpp>
+
+using namespace rustpp;
+
+auto add(u32 a, u32 b) -> u32 {
+    return a + b;
+}
+
+auto add(u32 a, u32 b, mut<u32&> out) {
+    out = a + b;
+}
+
+int main() {
+
+    // Mutable or not, must be initialised.
+    u32 a = 12;
+    u32 b = 24;
+
+    u32 sum1 = add(a, b);
+    fmt::print("Normal add: {} + {} == {}\n", a, b, sum1);
+
+    
+    // This fails, as wanted. Type must be mutable.
+    // u32 sum2 = 0;
+    // add(a, b, sum2);
+    
+    // This fails, as wanted. Passing must be explicit.
+    // mut<u32> sum3 = 0;
+    // add(a, b, sum3);
+
+    // Explicit and mutable.
+    mut<u32> sum4 = 0;
+    add(a, b, mut(sum4));
+
+    fmt::print("Exlicit add: {} + {} == {}\n", a, b, sum4);
+
+}
---
+++ include/rustpp/rustpp.hpp
@@ -0,0 +1,51 @@
+#include <type_traits>
+#include <cstdint>
+
+#pragma once
+
+namespace rustpp {
+
+	using u8  = const std::uint8_t;
+	using u16 = const std::uint16_t;
+	using u32 = const std::uint32_t;
+	using u64 = const std::uint64_t;
+	using i8  = const std::int8_t;
+	using i16 = const std::int16_t;
+	using i32 = const std::int32_t;
+	using i64 = const std::int64_t;
+	using f32 = const float;
+	using f64 = const double;
+	using usize = const std::size_t;
+	using isize = const std::ptrdiff_t;
+
+	template<typename T>
+	struct mut;
+
+	template<typename T> requires (std::is_reference_v<T> == false)
+	struct mut<T>
+	{
+		using type = std::decay_t<T>;
+		type t;
+		mut(const type &a) noexcept : t{ a } {}
+		mut &operator=(const type &a) noexcept { t = a; return *this; }
+		operator type&() noexcept { return t; }
+	};
+
+	template<typename T> requires std::is_reference_v<T>
+	struct mut<T>
+	{
+		using type = std::decay_t<T>;
+		type &t;
+		explicit mut(mut<const type> &a) noexcept : t{ a.t } {}
+		mut &operator=(const type &a) noexcept { t = a; return *this; }
+		operator type &() noexcept { return t; }
+	};
+
+	template<typename T>
+	std::ostream &operator<<(std::ostream &out, const mut<T> &m) {
+		return out << m.t;
+	}
+
+	template<typename T> mut(mut<T>&)->mut<T &>;
+
+} // namespace rustpp.
